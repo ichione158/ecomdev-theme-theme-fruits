211ebc186c76b78896e723fa043cf7c124355f21
hoand <hoan.duy1508@gmail.com>
2025-07-06 15:44:43 +0700

    Delete folder assets

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,17 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware(['web'])->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Public/Home', [
-            'theme' => 'theme-fruits',
-            'title' => 'Trang chủ',
-        ]);
-    });
+    // Route::get('/', function () {
+    //     return Inertia::render('Public/Home', [
+    //         'theme' => 'theme-fruits',
+    //         'title' => 'Trang chủ',
+    //     ]);
+    // });
 
-    Route::get('/about', function () {
-        return Inertia::render('Public/About', [
-            'theme' => 'theme-fruits',
-            'title' => 'Giới thiệu',
-        ]);
-    });
+    // Route::get('/about', function () {
+    //     return Inertia::render('Public/About', [
+    //         'theme' => 'theme-fruits',
+    //         'title' => 'Giới thiệu',
+    //     ]);
+    // });
 });
